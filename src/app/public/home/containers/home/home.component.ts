537b1d85cab19cb0a8298ffd304623984e8c674e
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-06-19 16:01:25 +0300

    feat(landing): landing was add

--- src/app/public/home/containers/home/home.component.ts
+++ src/app/public/home/containers/home/home.component.ts
@@ -2,11 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'home',
-  template: ``
+  styleUrls: ['home.component.scss'],
+  template: `
+    <div class="home">
+      <div class="container">
+        <div class="promo-info">
+          <div class="title">
+            <h1><div class="accent">One Mind </div> - лучший сервис для поиска финансирования</h1>
+          </div>
+          <div class="description">
+            OneMind создается, для получения и предоставления финансирования.
+          </div>
+          <div class="action">
+            <button class="primary" [routerLink]="['/search']">
+              Начать работу
+            </button>
+          </div>
+        </div>
+        <div class="promo-image">
+          <img src="../../../../../assets/images/common/promo.png" alt="" />
+        </div>
+      </div>
+    </div>
+  `,
 })
-
 export class HomeComponent implements OnInit {
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 }
